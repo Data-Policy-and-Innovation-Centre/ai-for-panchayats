41583b32b01bb52437ca815a88af85530527c3e2
vivek <vivekanandha.chowdary@gmail.com>
2026-06-18 11:20:46 +0000

    Updated the file path using the paths from config.py

--- scripts/run_sabhasaar.py
+++ scripts/run_sabhasaar.py
@@ -2,23 +2,17 @@ import sys
 from datetime import datetime
 from pathlib import Path
 
-# 1. Dynamically find the project root
-# __file__ = this script
-# .resolve() = gets the absolute, full path
-# .parent = the 'scripts' folder
-# .parent.parent = the 'ai-for-panchayats' root folder
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
-
-# 2. Adding the root to sys.path 
 sys.path.append(str(PROJECT_ROOT))
 
 from src.ingest.sabhasaar.config import SOURCE_PORTAL, STATE_CODE, HEADERS, FILTERS
 from src.ingest.sabhasaar.extractor import run_extraction
+from config import directories
 
 if __name__ == "__main__":
     RUN_DATE = datetime.now().strftime("%Y-%m-%d")
     
-    OUTPUT_DATASET = PROJECT_ROOT / "data" / "raw" / f"SabhaSaar_{RUN_DATE}"
+    OUTPUT_DATASET = directories.RAW_DATA / f"SabhaSaar_{RUN_DATE}"
     
     run_extraction(
         base_url=SOURCE_PORTAL,
